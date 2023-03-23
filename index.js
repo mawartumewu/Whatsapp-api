6a9b886ca931e2661c810aed4aa5b3350c393f13
EdsonSoumay <s21810088@student.unklab.ac.id>
2023-03-23 12:48:41 +0900

    update api

--- index.js
+++ index.js
@@ -1,63 +1,50 @@
-// versi api
-const express = require('express');
+// ini code fix
 const qrcode = require('qrcode-terminal');
+const fs = require("fs")
+const express = require('express');
 const { Client, LocalAuth } = require('whatsapp-web.js');
-const router = express.Router();
+
 const app = express();
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv').config();
+const port = process.env.PORT || 3000;
 
-const PORT = process.env.PORT;
-
-//cors setup
-app.use((req,res,next)=>{
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,DELETE,PUT');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Origin, Accept');
-  next()
-})
-
-app.use(bodyParser.json());
 const client = new Client({
-     authStrategy: new LocalAuth({
-          clientId: "client-one" 
-     }),
-     puppeteer: {
-      args: ['--no-sandbox'],
-    }
+  authStrategy: new LocalAuth({
+       clientId: "client-one" 
+  }),
+  puppeteer: {
+   args: ['--no-sandbox'],
+ }
 })
 
-router.post('/', (req, res) => {
-    const { wa_number, message} = req.body
-    console.log("req.body:",req.body);
-      /* const send_message = [
-         "6281287765680",
-         "6285244097783",
-     ]*/
-    client.on("ready", () => {
-        console.log("succesfully send")
-        wa_number.map(value => {
-            const chatId = value +"@c.us"
-            client.sendMessage(chatId,message)
-          })
-          res.status(200).send({
-             message: "succesfully send message",
-           });
-        })
-  });
-  
-  app.use('/wamessage', router);
+app.use(express.json());
+app.post('/send-message', async (req, res) => {
+    const { wa_numbers, message } = req.body
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}...`);
+    try {
+      wa_numbers.map(value => {
+                const chatId = value +"@c.us"
+                client.sendMessage(chatId,message);
+        })
+      res.send('Message sent successfully!');
+    } catch (error) {
+        res.status(500).send(error);
+    }
 });
 
 client.on("qr", qr => {
-  console.log(qr);
-  qrcode.generate(qr, {small: true} );
+    qrcode.generate(qr, {small: true} );
 })
 
 client.initialize();
+
+client.on("ready", () => {
+    console.log("WhatsApp client is ready!");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
 
 
 /////===================================
