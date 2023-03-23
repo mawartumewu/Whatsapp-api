4a2edc76d51ed13f7feab3de263acdff2945df94
EdsonSoumay <s21810088@student.unklab.ac.id>
2023-03-23 16:46:27 +0900

    add twetkjww

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
-// ini code fix
 const qrcode = require('qrcode-terminal');
 const express = require('express');
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const dotenv = require('dotenv').config();
 const app = express();
-const port = process.env.PORT
-
-let isClientReady = false;
-
+const port = process.env.PORT;
 const client = new Client({
   authStrategy: new LocalAuth({
-       clientId: "client-one" 
+    clientId: "client-one"
   }),
   puppeteer: {
-   args: ['--no-sandbox'],
- }
-})
+    args: ['--no-sandbox'],
+  }
+});
 
+function keepSessionAlive() {
+  if (!client.isConnected()) {
+    client.connect();
+  }
+}
 
 app.use(express.json());
+
 app.post('/send-message', async (req, res) => {
-  const { wa_numbers, message } = req.body;
+  const { wa_numbers, message } = req.body
   try {
-    if (isClientReady) {
-      wa_numbers.map((value) => {
-        const chatId = value + '@c.us';
-        client.sendMessage(chatId, message);
-      });
-      res.status(200).send({message:'Message sent successfully!'});
-    } else {
-      res.status(200).send({message:'WhatsApp client is not ready. Please wait until it is ready!'});
-    }
+    wa_numbers.map(value => {
+      const chatId = value +"@c.us"
+      client.sendMessage(chatId,message);
+    })
+    res.send('Message sent successfully!');
   } catch (error) {
     res.status(500).send(error);
   }
@@ -38,18 +36,73 @@ app.post('/send-message', async (req, res) => {
 
 client.on("qr", qr => {
   console.log(qr);
-    qrcode.generate(qr, {small: true} );
-})
+  qrcode.generate(qr, {small: true} );
+});
 
 client.initialize();
 
-client.on('ready', () => {
-  isClientReady = true;
-  console.log('WhatsApp client is ready!');
+client.on("ready", () => {
+  console.log("WhatsApp client is ready!");
+  setInterval(keepSessionAlive, 60000); // Call every minute
 });
+
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
+
+
+// // ini code fix
+// const qrcode = require('qrcode-terminal');
+// const express = require('express');
+// const { Client, LocalAuth } = require('whatsapp-web.js');
+// const dotenv = require('dotenv').config();
+// const app = express();
+// const port = process.env.PORT
+
+// let isClientReady = false;
+
+// const client = new Client({
+//   authStrategy: new LocalAuth({
+//        clientId: "client-one" 
+//   }),
+//   puppeteer: {
+//    args: ['--no-sandbox'],
+//  }
+// })
+
+
+// app.use(express.json());
+// app.post('/send-message', async (req, res) => {
+//   const { wa_numbers, message } = req.body;
+//   try {
+//     if (isClientReady) {
+//       wa_numbers.map((value) => {
+//         const chatId = value + '@c.us';
+//         client.sendMessage(chatId, message);
+//       });
+//       res.status(200).send({message:'Message sent successfully!'});
+//     } else {
+//       res.status(200).send({message:'WhatsApp client is not ready. Please wait until it is ready!'});
+//     }
+//   } catch (error) {
+//     res.status(500).send(error);
+//   }
+// });
+
+// client.on("qr", qr => {
+//   console.log(qr);
+//     qrcode.generate(qr, {small: true} );
+// })
+
+// client.initialize();
+
+// client.on('ready', () => {
+//   isClientReady = true;
+//   console.log('WhatsApp client is ready!');
+// });
+// app.listen(port, () => {
+//     console.log(`Server running on port ${port}`);
+// });
 
 
 
