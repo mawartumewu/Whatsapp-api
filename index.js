cee12502551fe2999dd95ff847bff81d18c7dd76
EdsonSoumay <s21810088@student.unklab.ac.id>
2023-03-26 11:28:36 +0900

    update lagi

--- index.js
+++ index.js
@@ -112,23 +112,48 @@ const mongoose = require('mongoose');
 
 let isClientReady = false;
 
+function clientSetUp (client){
+  client.on('authenticated', () => {
+    console.log('Client is authenticated!');
+  });
+  
+  client.on('qr', (qr) => {
+    console.log('QR code generated!');
+    qrcode.generate(qr, { small: true });
+  });
+
+  client.on('remote_session_saved', async () => {
+    console.log('Session saved!');
+  });
+
+  client.on('ready', () => {
+    isClientReady = true;
+    console.log('WhatsApp client is ready!');
+  });
+
+  client.on('auth_failure', (err) => {
+    console.error('Authentication failure:', err);
+  });
+
+  client.on('disconnected', (reason) => {
+    isClientReady = false;
+    console.log('Client was disconnected:', reason);
+    clientSetUp(client)
+  });
+
+  client.initialize();
+}
+
 // Load the session data
 mongoose.connect(moongodburl)
   .then(() => {
        const store = new MongoStore({ mongoose: mongoose });
-    const client = new Client({
-        authStrategy: new RemoteAuth({
-            store: store,
-            backupSyncIntervalMs: 300000
-        })
-    });
-
-    client.on('qr', (qr) => {
-      console.log('QR code generated!');
-      qrcode.generate(qr, { small: true });
-      // jika perlu, Anda bisa menambahkan tindakan selanjutnya setelah QR code tergenerate
-      // misalnya menampilkan QR code di browser atau dalam aplikasi desktop
-    });
+        const client = new Client({
+            authStrategy: new RemoteAuth({
+                store: store,
+                backupSyncIntervalMs: 300000,
+            })
+        });
 
     app.use(express.json());
     app.post('/send-message', async (req, res) => {
@@ -148,25 +173,8 @@ mongoose.connect(moongodburl)
       }
     });
 
-    client.on('remote_session_saved', async () => {
-      console.log('Session saved!');
-    });
-
-    client.on('ready', () => {
-      isClientReady = true;
-      console.log('WhatsApp client is ready!');
-    });
-
-    client.on('auth_failure', (err) => {
-      console.error('Authentication failure:', err);
-    });
-
-    client.on('disconnected', (reason) => {
-      isClientReady = false;
-      console.log('Client was disconnected:', reason);
-    });
-
-    client.initialize();
+    clientSetUp(client)
+  
   })
   .catch((err) => {
     console.error('MongoDB connection error:', err);
@@ -175,70 +183,3 @@ mongoose.connect(moongodburl)
   app.listen(port, () => {
   console.log(`Server running on port ${port}`);
   });
-
-
-
-// // versi save remote sesion simpplifly
-// const express = require('express');
-// const qrcode = require('qrcode-terminal');
-// const { Client, RemoteAuth } = require('whatsapp-web.js');
-// const dotenv = require('dotenv').config();
-// const app = express();
-// const port = process.env.PORT;
-// const moongodburl = process.env.MONGODB_URI;
-
-// // Require database
-// const { MongoStore } = require('wwebjs-mongo');
-// const mongoose = require('mongoose');
-
-// let isClientReady = false;
-
-// // Load the session data
-// mongoose.connect(moongodburl)
-//   .then(() => {
-//        const store = new MongoStore({ mongoose: mongoose });
-//     const client = new Client({
-//         authStrategy: new RemoteAuth({
-//             store: store,
-//             backupSyncIntervalMs: 300000,
-//             session: { path: './sessions' },
-//         })
-//     });
-
-//     client.on('qr', (qr) => {
-//       console.log('QR code generated!');
-//       qrcode.generate(qr, { small: true });
-//     });
-
-//     app.use(express.json());
-//     app.post('/send-message', async (req, res) => {
-//       const { wa_numbers, message } = req.body;
-//       try {
-//         if (isClientReady) {
-//           wa_numbers.map((value) => {
-//             const chatId = value + '@c.us';
-//             client.sendMessage(chatId, message);
-//           });
-//           res.status(200).send({message:'Message sent successfully!'});
-//         } else {
-//           res.status(200).send({message:'WhatsApp client is not ready. Please wait until it is ready!'});
-//         }
-//       } catch (error) {
-//         res.status(500).send(error);
-//       }
-//     });
-
-//     client.on('remote_session_saved', async () => {
-//       console.log('Session saved!');
-//     });
-    
-//     client.initialize();
-//   })
-//   .catch((err) => {
-//     console.error('MongoDB connection error:', err);
-//   });
-  
-//   app.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
-//   });
-
